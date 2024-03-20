185cefae705ae36389350c0416b01701250a560c
unknown <shahaparan2014@gmail.com>
2024-03-19 23:54:40 -0400

    fixed

--- public_tests.py
+++ public_tests.py
@@ -24,21 +24,19 @@ def test_model(target, classes, input_size):
     
     assert len(target.layers) == 3, \
         f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
-    assert target.input.shape.as_list() == [None, input_size], \
-        f"Wrong input shape. Expected [None,  {input_size}] but got {target.input.shape.as_list()}"
+    assert target.input_shape == (None, input_size), \
+        f"Wrong input shape. Expected [None,  {input_size}] but got {target.input_shape}"
     i = 0
-    expected = [[Dense, [None, 25], relu],
-                [Dense, [None, 15], relu],
-                [Dense, [None, classes], linear]]
-
+    expected = [[Dense, (None, 25), relu],
+                [Dense, (None, 15), relu],
+                [Dense, (None, classes), linear]]
     for layer in target.layers:
         assert type(layer) == expected[i][0], \
             f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
-        assert layer.output.shape.as_list() == expected[i][1], \
-            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
+        assert layer.output.shape == expected[i][1], \
+            f"Wrong shape in layer {i}. Expected {expected[i][1]} but got {layer.output.shape}"
         assert layer.activation == expected[i][2], \
             f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
         i = i + 1
 
     print("\033[92mAll tests passed!")
-    
